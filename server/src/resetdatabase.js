87b78b8e6b2059684053c5e3080292ac3189f2a6
Yi Fung <yirenfung@gmail.com>
2017-04-17 14:42:51 -0400

    databased

--- server/src/resetdatabase.js
+++ server/src/resetdatabase.js
@@ -143,12 +143,19 @@ function resetDatabase(db, cb) {
       // Use myself as a callback.
       resetCollection(db, collection, processNextCollection);
     } else {
-      cb();
+      addIndexes(db, cb);
     }
   }
 
   // Start processing the first collection!
   processNextCollection();
+}
+
+/**
+ * Adds any desired indexes to the database.
+ */
+function addIndexes(db, cb) {
+  db.collection('feedItems').createIndex({ "contents.contents": "text" }, null, cb);
 }
 
 // Check if called directly via 'node', or required() as a module.
